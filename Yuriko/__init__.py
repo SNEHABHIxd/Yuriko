aef7a9a6900c8279d4a09000b914ffdd2ea11790
SNEHABHIxd <93216312+SNEHABHIxd@users.noreply.github.com>
2022-01-04 12:49:39 +0530

    Update __init__.py

--- Yuriko/__init__.py
+++ Yuriko/__init__.py
@@ -130,7 +130,7 @@ else:
 
     JOIN_LOGGER = Config.JOIN_LOGGER
     OWNER_USERNAME = Config.OWNER_USERNAME
-    ALLOW_CHATS = Config.ALLOW_CHATS
+    ALLOW_CHATS = Config.ALLOW_EXCL
     try:
         DRAGONS = {int(x) for x in Config.DRAGONS or []}
         DEV_USERS = {int(x) for x in Config.DEV_USERS or []}
